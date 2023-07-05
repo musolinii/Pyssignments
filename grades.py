5ed27152fca3e243a8691961ee18a7d668fc416f
musolinii <brianbwana7265@gmail.com>
2023-07-05 18:20:56 +0300

    initial commit

--- grades.py
+++ grades.py
@@ -1,15 +1,15 @@
-def grades(grade) :
+def grades(name, grade) :
 
     if grade < 40 :
-        print('You got an E')
+        print(name + ' you got an E')
     elif grade > 39 and  grade < 44 : 
-        print('You got a D')
+        print(name + ' you got a D')
     elif grade > 45 and grade < 50  :
-        print('You got a C')
+        print(name + ' you got a C')
     elif grade > 49 and grade < 60 :
-        print('You got a C')
+        print(name + ' you got a C')
     elif grade > 59 and grade < 90 :
-        print('You got a B')
-    else : print('You got an A!')
+        print(name + ' you got a B')
+    else : print(name + ' you got an A!')
 
-grades(97)
+grades("Rick",10)
--- prime.py
+++ prime.py
@@ -1,14 +1,18 @@
-prime = []
-for i in range (0,101):
+def primenumbers(n):
 
-    count = 0
+    prime = []
+    for i in range (0,n + 1):
 
-    for j in range(1,i):
-        
-        if i % j == 0:
-            count += 1
+        count = 0
+
+        for j in range(1,i):
             
-    if count == 1 :
-        prime.append(i)
+            if i % j == 0:
+                count += 1
 
-print(prime)
+        if count == 1 :
+            prime.append(i)
+
+    print(prime)
+
+primenumbers(10)
